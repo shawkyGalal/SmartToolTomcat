6ba29efcb9faece2106ed2c06914727034c04338
shawkyGalal <shawky.foda@gmail.com>
2018-10-26 00:27:19 +0300

    6th commit

---
+++ Company/20/Testing/templates/includes/rightbar-sparklines.blade.php
@@ -0,0 +1,49 @@
+<div class="infobar scroll-content">
+  <div id="widgetarea">
+    <div id="widget-sparkline" class="widget">
+      <div class="widget-heading">
+        <a data-target="#sparklinestats" data-toggle="collapse" href="javascript:;"><h4>Sparkline Stats</h4></a>
+      </div>
+      <div class="collapse in" id="sparklinestats">
+        <div class="widget-body">
+          <div class="widget-block clearfix">
+            <div class="pull-left">
+              <h5 class="mb-sm">Credit Balance</h5>
+              <span class="big">$7,504</span>
+            </div>
+            <div class="pull-right mt-md">
+              <div id="pierightbar"></div>
+            </div>
+          </div>
+          <div class="widget-block clearfix">
+            <div class="pull-left">
+              <h5 class="mb-sm">Sales Volume</h5>
+              <span class="big">$164,910</span>
+            </div>
+            <div class="pull-right mt-md">
+              <div id="barrightbar"></div>
+            </div>
+          </div>
+          <div class="widget-block clearfix">
+            <div class="pull-left">
+              <h5 class="mb-sm">Sales Volume</h5>
+              <span class="big">$164,910</span>
+            </div>
+            <div class="pull-right mt-md">
+              <div id="linerightbar"></div>
+            </div>
+          </div>
+          <div class="widget-block clearfix">
+            <div class="pull-left">
+              <h5 class="mb-sm">Sales Volume</h5>
+              <span class="big">$164,910</span>
+            </div>
+            <div class="pull-right mt-md">
+              <div id="tristate"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
